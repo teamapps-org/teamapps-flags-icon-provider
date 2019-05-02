38bab11c163aaa15521595cd60a780e3d8200da1
Yann Massard <yann.massard@teamapps.org>
2019-05-02 17:33:38 +0200

    rename package

--- src/main/java/org/teamapps/icon/flags/FlagsIconProvider.java
+++ src/main/java/org/teamapps/icon/flags/FlagsIconProvider.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  * =========================LICENSE_END==================================
  */
-package org.teamapps.icon.flag;
+package org.teamapps.icon.flags;
 
 import org.apache.commons.io.IOUtils;
 import org.teamapps.icons.api.IconStyle;
